9d684bd24944fd8239951b44f31060372a06d49c
Christopher Cabrera <ccabrera622@gmail.com>
2020-10-20 11:54:16 -0400

    .Net Intro

---
+++ 02DotNetIntro/PluginBase/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginBase
+{
+    public interface ICommand
+    {
+        string Name { get; }
+        string Description { get; }
+
+        int Execute();
+    }
+}
